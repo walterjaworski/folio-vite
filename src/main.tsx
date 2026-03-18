9f5906b0afd931388e40fac10814b816f647b87d
Walter Jaworski <walter.jaworski@gmail.com>
2026-03-18 16:14:08 -0300

    feat: create queryClient, add query provider and query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools/production'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './index.css'
+import { queryClient } from './shared/lib/react-query/queryClient.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 )
