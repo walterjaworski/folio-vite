eb102c77b2fbc06697e0dbf54979ab09d28cd289
Walter Jaworski <walter.jaworski@gmail.com>
2026-03-31 09:57:13 -0300

    style: load queryClient from new folder

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { queryClient } from './shared/lib/react-query/queryClient.ts'
+import { queryClient } from './lib/queryClient.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
