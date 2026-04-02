2bf328bad87d024167f0bc0df624d11f01e8bb2a
Walter Jaworski <walter.jaworski@gmail.com>
2026-04-02 11:04:18 -0300

    style: change classes on PageHeader component

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -8,8 +8,13 @@ export default function PageHeader({ title, description, action }: IPageHeaderPr
   return (
     <div className="flex items-center justify-between mb-8">
       <div>
-        <h1 className="text-5xl font-title font-extrabold tracking-tight text-neutral-900">{title}</h1>
-        {description && <p className="text-muted-foreground mt-1 text-lg">{description}</p>}
+        <h1 className="text-5xl font-title font-extrabold tracking-tight text-foreground transition-colors duration-200">
+          {title}
+        </h1>
+        {description &&
+          <p className="text-muted-foreground mt-1 text-lg transition-colors duration-200">
+            {description}
+          </p>}
       </div>
       {action && action}
     </div>
